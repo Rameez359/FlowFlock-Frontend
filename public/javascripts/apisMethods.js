ddc44b2eb2e98e6f1cf7197528c43cc90f6cfec3
Rameez Ali <alirameez359@gmail.com>
2024-01-20 03:41:36 +0500

    [ff-signup] - Integrate Gmail account to send verification code

--- public/javascripts/apisMethods.js
+++ public/javascripts/apisMethods.js
@@ -5,9 +5,13 @@ const postData = async (url, data) => {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(data),
         });
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
 
         const result = await response.json();
         console.log(`Local Signup StepOne Ended with Response: [${JSON.stringify(result)}]`);
+        return result;
     } catch (error) {
         console.error('Error:', error);
     }
