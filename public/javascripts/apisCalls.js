4a8769739cb991f3e51aec04f0a2d8af5cab2250
Rameez Ali <alirameez359@gmail.com>
2024-01-24 23:22:02 +0500

    [ff-modal] - Create sin up step 3, choose username

--- public/javascripts/apisCalls.js
+++ public/javascripts/apisCalls.js
@@ -21,18 +21,34 @@ const registerStepOne = async () => {
     }
     document.getElementById('modal-content-step-one').classList.add('display-none');
     document.getElementById('modal-content-step-two').classList.remove('display-none');
-
-    span = document.getElementsByClassName('close')[0];
 };
-const registerStepTwo = async() => {
+
+const registerStepTwo = async () => {
     const code = document.getElementById('verification-code').value;
     const userId = localStorage.getItem('userId');
     const data = {
-        verificationCode : code,
-        userId : userId
-    }
+        code: code,
+        userId: userId,
+    };
 
     const response = await postData('http://localhost:3000/localSignupStepTwo', data);
+    console.log('Success:', response);
+
+    if (response.statusCode === 200) {
+        document.getElementById('modal-content-step-two').classList.add('display-none');
+        document.getElementById('modal-content-step-three').classList.remove('display-none');
+    }
+};
+
+const registerStepThree = async () => {
+    const username = document.getElementById('username').value;
+    const userId = localStorage.getItem('userId');
+    const data = {
+        username: username,
+        userId: userId,
+    };
+
+    const response = await postData('http://localhost:3000/localSignupStepThree', data);
     console.log('Success:', response);
 
 };
